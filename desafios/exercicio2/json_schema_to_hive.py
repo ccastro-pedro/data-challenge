f7295cb670b182994d1c6c25e92c2cba2dbd1c37
ccastro <pedro.castro@cinnecta.com>
2021-08-15 11:24:09 -0300

    tentativa 1 de execução do exercicio 2

--- desafios/exercicio2/json_schema_to_hive.py
+++ desafios/exercicio2/json_schema_to_hive.py
@@ -1,25 +1,132 @@
+import os
+
+from exercicio1.event_validator import JsonValidator
+
 _ATHENA_CLIENT = None
 
+
+class SchemaToTable(JsonValidator):
+    schema = None
+    cols = None
+    default = "s3://bucket/iti-query/"
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    @classmethod
+    def _get_col_names_and_types(cls, fields):
+        # cols = {}
+        # for i, v in fields.items():
+        #     if isinstance(v, dict):
+        #         for j, k in v.items():
+        #             if j == 'type' and k != 'object':
+        #                 cols[i] = {'type': k, 'description': ''}
+        #             elif j == 'fields':
+        #                 for x, y in k.items():
+        #                     cols[f'{i}_{x}'] = y.get('type')
+        cols = {}
+        for i, v in fields.items():
+            if isinstance(v, dict):
+                for j, k in v.items():
+                    if j == 'type':
+                        cols[i] = k
+                    elif j == 'fields':
+                        for x, y in k.items():
+                            cols[f'{i}_{x}'] = y.get('type')
+        # print(fields)
+        cls.cols = fields
+
+    @classmethod
+    def _get_description(cls):
+        for k in cls.cols.keys():
+            print(k, cls.schema.get('properties').get(k).get('description'))
+
+    @classmethod
+    def _convert_types(cls, typ):
+        return {
+            'integer': 'INT',
+            'string': 'STRING',
+            'object': 'STRUCT',
+            'array': 'ARRAY',
+            'boolean': 'BOOLEAN',
+            'null': 'NULL'
+        }.get(typ)
+
+    @classmethod
+    def _format_columns_type(cls, i, v):
+        return f"{i} {cls._convert_types(v.get('type'))}"
+
+    @classmethod
+    def _create_cols_string(cls, fields, cols_string=''):
+        # print([y for x, y in enumerate(cols_string.split()) if not x%2])
+        while len(fields) > 0:
+            for i, v in list(fields.items()):
+                print(len(fields))
+                if v.get('type') != 'object':
+                    cols_string += cls._format_columns_type(i, v) + ',\n\t\t'
+                    fields.pop(i, None)
+                else:
+                    cols_string += cls._format_columns_type(i, v)
+                    aux_fields = v.get("fields", {})
+                    fields.pop(i, None)
+                    return cls._create_cols_string(aux_fields, cols_string + "<")
+            else:
+                cols_string = cols_string[:len(cols_string) - 2]
+            cols_string += ">"
+        return cols_string
+
+    @classmethod
+    def get_create_table_query(cls, location=default):
+        cls._required_fields_and_types()
+        cls._get_col_names_and_types(cls.fields)
+        # cls._get_description()
+
+        # cols_string = cls._create_cols_string(cls.cols)
+
+        cols_string = ',\n\t\t'.join(cls._format_columns_type(i, v) if v.get('type') != 'object'
+                                     else cls._format_columns_type(i, v) +
+                                          "<" + f",\n\t\t{' ' * len(cls._format_columns_type(i, v))}"
+                                     .join(cls._format_columns_type(j, k) for j, k in v.get("fields", {}).items()) +
+                                          f"\n\t\t{' ' * len(cls._format_columns_type(i, v))}>" for i, v in
+                                     cls.cols.items())
+
+        query = f"""
+        CREATE EXTERNAL TABLE IF NOT EXISTS
+        json_schema_table (
+        {cols_string}
+        )
+        ROW FORMAT SERDE 'org.openx.data.jsonserde.JsonSerDe'
+        LOCATION {location}         
+        """
+
+        return query
+
+
 def create_hive_table_with_athena(query):
-    '''
+    """
     Função necessária para criação da tabela HIVE na AWS
     :param query: Script SQL de Create Table (str)
     :return: None
-    '''
-    
+    """
+
     print(f"Query: {query}")
-    _ATHENA_CLIENT.start_query_execution(
+    ret = _ATHENA_CLIENT.start_query_execution(
         QueryString=query,
         ResultConfiguration={
             'OutputLocation': f's3://iti-query-results/'
         }
     )
+    print(ret)
+
 
 def handler():
-    '''
+    """
     #  Função principal
     Aqui você deve começar a implementar o seu código
     Você pode criar funções/classes à vontade
     Utilize a função create_hive_table_with_athena para te auxiliar
         na criação da tabela HIVE, não é necessário alterá-la
-    '''
+    """
+    SchemaToTable.get_schema('./schema.json')
+
+    query = SchemaToTable.get_create_table_query()
+
+    create_hive_table_with_athena(query)
